5d7593ac782899079e3eed42f6cb07c445569b29
nvankems <nvankems@yahoo.com>
2016-02-15 11:25:24 -0600

    finished

--- closures.js
+++ closures.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+console.log(inner());
 
 
 
@@ -21,18 +21,19 @@ var outer = function(){
 
 
 
-var callFriend = function(){
+var callFriend = function(number){
   var friend = 'Jake';
-  function callF(number){
+  function callF(){
     return 'Calling ' + friend + ' at ' + number;
   }
-  return callF;
+  return callF();
 };
 
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+console.log(callFriend('435-215-9248'));
+console.log(callFriend('787-407-1451'));
 
 
 
@@ -44,12 +45,18 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
-  //Code Here
+
+function makeCounter() {
+  var i = 1;
+    return function() {
+      return i++;
+}
+}
   var count = makeCounter();
-  count(); // 1
-  count(); // 2
-  count(); // 3
-  count(); // 4
+  console.log(count()); // 1
+  console.log(count()); // 2
+  console.log(count()); // 3
+  console.log(count()); // 4
 
 
 
@@ -58,70 +65,138 @@ var callFriend = function(){
 
 
 /*
-  Write a function named codeLove that returns the string 'I love code'. Write a second function named codeFriend that accepts the first function as it's first parameter. The second function should return a new third function. Store the third function in a variable, codeEcho which, when invoked, invokes the first, original function that was passed in, but will only ever do so once (returns null after first invocation).
-*/
-
-  //Code Here
-
-
-
-//Next Problem
-
-
-
-/*
-  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
+  Write a function named codeLove that returns the string 'I love code'. Write a second function named codeFriend that accepts the first function as it's first parameter. 
+  The second function should return a new third function. 
+  Store the third function in a variable, codeEcho which, when invoked, invokes the first, original function that was passed in, but will only ever do so once (returns null after first invocation).
 */
 
 
+function codeLove() {
+    return 'I love code';
+}
+  function codeFriend (codeLove) {
+    var i = 0;
+     return function() {
+      i++;
+      if (i === 1) {
+        return codeLove();
+      }
+      else {
+        return null;
+      }
+      
+     }
+  }
 
+
+var codeEcho = codeFriend(codeLove);
+console.log(codeEcho());
+console.log(codeEcho());
+console.log(codeEcho());
 //Next Problem
 
 
 
 /*
-  var counter = function(){
-    for (var i=1; i<=5; i++) {
-      setTimeout( function timer(){
-          console.log( i );
-      }, i*1000 );
+  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. 
+  The first parameter will be an anonymous function and the second parameter, 'N', will be a number. 
+  Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
+*/
+
+function fnCounter (F, N) {
+  var i = 0;
+  return function() {
+    if (i < N) {
+      i++
+      return F();
     }
-  };
+    else {
+      return 'STOP';
+    }
+  }
+}
 
-  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
+function test() {
+  return "I hate code";
+}
 
-    //Answer Here
+var counterEcho = fnCounter(test, 3)
+console.log(counterEcho());
+console.log(counterEcho());
+console.log(counterEcho());
+console.log(counterEcho());
+console.log(counterEcho());
+
+
+//Next Problem
+
+
+
+
+//   var counter = function(){
+//     for (var i=1; i<=5; i++) {
+//       setTimeout( function timer(){
+//           console.log( i );
+//       }, i*1000 );
+//     }
+//   };
+// counter();
+/*  
+Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. 
+  *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
+
+    //It will console log i every 1000ms
 
 
   Now, run the function in your console and note what happpens.
 
   Was your answer right or wrong?
 
-    //Answer Here
+    //Wrong
 
 
   Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc) (Note: No unit test for this one because of the timeout)
 */
 
-    //Code Here
-
+//     var counter = function() {
+//       for (i = 1; i <=5; i++) {
+//         setTimeout(, 1000)
+//       }
+//     }
+// counter();
 
 
 //Next Problem
 
+function makeArrays() {
+  var arr = [];
 
+  for (var i = 0; i < 6; i++) {
 
-/*
-  Make the following code work
+    arr.push(
+        (function (j){
+            return function() {
+              console.log(j);
+            }
+          }(i))
+      )
 
-  funcArray[0]() //0
-  funcArray[1]() //1
-  funcArray[2]() //2
-  funcArray[3]() //3
-  funcArray[4]() //4
-  funcArray[5]() //5
+  }
 
-  *Hint: Don't let this fool you. Break down what's really happening here.
-*/
+  return arr;
+}
+
+var funcArray = makeArrays();
+  // Make the following code work
+
+  funcArray[0](); //0
+  funcArray[1](); //1
+  funcArray[2](); //2
+  funcArray[3](); //3
+  funcArray[4](); //4
+  funcArray[5](); //5
+
+  // *Hint: Don't let this fool you. Break down what's really happening here.
+
 
 
--- fns-and-scope.js
+++ fns-and-scope.js
@@ -1,81 +1,102 @@
-//Once you complete a problem, open up Chrome and check the answer in the console.
+// //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-var name = 'Tyler';
-//Create a function called isTyler that accepts name as it's only parameter.
-//If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
+// var name = 'Tyler';
+// //Create a function called isTyler that accepts name as it's only parameter.
+// //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
-
-//Next problem
-
-
-
-//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
-
-  //Code Here
-
-
-//Next Problem
+// function isTyler(name) {
+//  if (name === 'Tyler') {
+//  	return true;
+//  }
+// }
+  
+// console.log(isTyler(name));//Next problem
 
 
 
-//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
-//then alerts "Welcome, " plus whatever the users name is.
-
-  //Code Here
+// //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-//Next problem
+//   function  getName (name) {
+//   name = prompt("Enter your name");
+//   return name;
+// }
+// // getName();
 
-
-
-
-//What is the difference between arguments and parameters?
-
-  //Answer Here
-
-
-//Next problem
+// //Next Problem
 
 
 
-//What are all the falsy values in JavaScript and how do you check if something is falsy?
+// //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
+// //then alerts "Welcome, " plus whatever the users name is.
+
+// function welcome() {
+// 	return alert("Welcome " + getName());
+// }
+
+// welcome();
 
 
-  //Answer Here
+// //Next problem
 
 
 
-//Next Problem
+
+// //What is the difference between arguments and parameters?
+
+//   //Parameters are variables in the call field of a function, arguments are the variables called inside the function.
+
+
+// //Next problem
 
 
 
-//Create a function called myName that returns your name
+// //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
-  //Code Here
+
+//   //Falsy values: NaN, undefined, false, 0, '', null. To check if something is falsy use !variable.
+
+
+
+// //Next Problem
+
+
+
+// //Create a function called myName that returns your name
+
+// function myName (name) {
+// 	return 'Nick';
+// 	 // body...  
+// }
+// console.log(myName());
   
 
 
-//Now save the function definition of myName into a new variable called newMyName
+// //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+// var newMyName = myName();
 
-//Now alert the result of invoking newMyName
+// //Now alert the result of invoking newMyName
+
+// alert(newMyName);
+
+// //Next problem
 
 
 
-//Next problem
+// //Create a function called outerFn which returns an anonymous function which returns your name.
 
+// function outerFn () {
+// 	return function() {
+// 		return 'Nick';
+// 	}
+// }
 
+// //Now save the result of invoking outerFn into a variable called innerFn.
 
-//Create a function called outerFn which returns an anonymous function which returns your name.
+// var innerFn = outerFn();
 
-  //Code Here
+// //Now invoke innerFn.
 
-//Now save the result of invoking outerFn into a variable called innerFn.
-
-  //Code Here
-
-//Now invoke innerFn.
+// console.log(innerFn());
